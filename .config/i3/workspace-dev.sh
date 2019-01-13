f18ce1cac724b89f4c247f0cf0bb5d7ded120fa4
Nicole Nisters <n.nisters@pm.me>
2019-01-13 03:00:55 +0100

    Move dev workspace to 2

--- .config/i3/workspace-dev.sh
+++ .config/i3/workspace-dev.sh
@@ -1,8 +1,8 @@
-if i3-msg -t get_workspaces | grep "1: dev"; then
-    i3-msg "workspace 1: dev"
+if i3-msg -t get_workspaces | grep "2: dev"; then
+    i3-msg "workspace 2: dev"
 else
     # Load layout
-    i3-msg "workspace 1: dev; append_layout ~/.config/i3/dev/workspace-dev.json"
+    i3-msg "workspace 2: dev; append_layout ~/.config/i3/dev/workspace-dev.json"
     
     # Start applications
     (alacritty --class task-container -e ".config/i3/dev/task-dev.sh" &)
