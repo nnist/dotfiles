c5fd4ac7c41cdf42ebd012b21784a138fec27a28
Nicole Nisters <n.nisters@pm.me>
2019-01-16 00:00:11 +0100

    Use iftop instead of ifconfig

--- .config/i3/system/ifconfig.sh
+++
@@ -1,3 +0,0 @@
-source ~/.config/base16-shell/scripts/base16-default-dark.sh
-echo "Please enter your password to use ifconfig."
-sudo ifconfig
---
+++ .config/i3/system/iftop.sh
@@ -0,0 +1,3 @@
+source ~/.config/base16-shell/scripts/base16-default-dark.sh
+echo "Please enter your password to use iftop."
+sudo iftop
