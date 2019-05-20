19c6c19ae745f5588ef94df46955d3d8c3b78822
Nicole Nisters <n.nisters@pm.me>
2019-05-21 00:43:20 +0200

    i3: Run taskwarrior on startup to hide recur msgs

--- i3/tasks/tasks.sh
+++ i3/tasks/tasks.sh
@@ -1,4 +1,8 @@
 cd ~/.task
+
+# Run taskwarrior once to trigger recurrences, and mute the output
+task > /dev/null 2>&1
+
 printf "Most urgent tasks:\n"
 task +urgent rc.context=none 2> /dev/null
 printf "\n\nScheduled tasks for today:\n"
