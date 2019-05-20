0a8703591565f5de0eac3acc5c33bb83f6971384
Nicole Nisters <n.nisters@pm.me>
2019-05-21 00:33:25 +0200

    i3: Clean up tasks output further

--- i3/tasks/tasks.sh
+++ i3/tasks/tasks.sh
@@ -1,7 +1,7 @@
 cd ~/.task
-printf "Most urgent tasks:\n\n"
-task +urgent rc.context=none
-printf "\n\nScheduled tasks for today:\n\n"
-task schedule rc.context=none
+printf "Most urgent tasks:\n"
+task +urgent rc.context=none 2> /dev/null
+printf "\n\nScheduled tasks for today:\n"
+task schedule rc.context=none 2> /dev/null
 printf "\n\n"
 bash
