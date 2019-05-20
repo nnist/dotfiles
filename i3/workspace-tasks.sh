c05a6601c130ac663be8595d2363ee77c34f4236
Nicole Nisters <n.nisters@pm.me>
2019-05-20 14:29:24 +0200

    i3: Add clock to tasks workspace

--- i3/workspace-tasks.sh
+++ i3/workspace-tasks.sh
@@ -8,6 +8,7 @@ else
     (alacritty -d 0 0 --class task-container -e ".config/i3/tasks/tasks.sh" &)
     sleep .5
     (alacritty -d 0 0 --working-directory ~/ &)
+    (alacritty -d 0 0 --class task-clock-container -e ".config/i3/tasks/task-clock.sh" &)
     (alacritty -d 0 0 --class task-calendar-container -e ".config/i3/tasks/task-calendar.sh" &)
     (alacritty -d 0 0 --class vim-inbox-container -e ".config/i3/tasks/vim-inbox.sh" &)
 fi
