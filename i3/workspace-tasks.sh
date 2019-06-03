f32cbbafe00c8bb26fa4451ce5fd5c7d85f7bfa1
Nicole Nisters <n.nisters@pm.me>
2019-06-03 15:26:29 +0200

    Display clock instead of calendar by default

--- i3/workspace-tasks.sh
+++ i3/workspace-tasks.sh
@@ -5,11 +5,11 @@ else
     i3-msg "workspace 1: tasks; append_layout ~/.config/i3/tasks/workspace-tasks.json"
     
     # Start applications
+    (alacritty -d 0 0 --title "calendar" --class task-calendar-container -e ".config/i3/tasks/task-calendar.sh" &)
     (alacritty -d 0 0 --class task-schedule-container -e ".config/i3/tasks/task-schedule.sh" &)
     (alacritty -d 0 0 --class task-container -e ".config/i3/tasks/tasks.sh" &)
     sleep .5
     (alacritty -d 0 0 --working-directory ~/ &)
-    (alacritty -d 0 0 --title "clock" --class task-clock-container -e ".config/i3/tasks/task-clock.sh" &)
-    (alacritty -d 0 0 --title "calendar" --class task-calendar-container -e ".config/i3/tasks/task-calendar.sh" &)
     (alacritty -d 0 0 --class vim-inbox-container -e ".config/i3/tasks/vim-inbox.sh" &)
+    (alacritty -d 0 0 --title "clock" --class task-clock-container -e ".config/i3/tasks/task-clock.sh" &)
 fi
