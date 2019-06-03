b7a79d1edaa920a1f24b25ed5208a6db261b307c
Nicole Nisters <n.nisters@pm.me>
2019-06-03 21:27:49 +0200

    Auto open scrap.md file in gtd dir

--- i3/tasks/vim-inbox.sh
+++ i3/tasks/vim-inbox.sh
@@ -1,4 +1,4 @@
 source .config/base16-shell/scripts/base16-default-dark.sh
 cd ~/gtd
-vim inbox/inbox.md inbox/incubate.md inbox/reference.md
+vim scrap.md inbox/inbox.md inbox/incubate.md inbox/reference.md
 bash
