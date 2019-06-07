2e8fe2b8118c77c3bcfbe4e3f3a7e674a91a1579
Nicole Nisters <n.nisters@pm.me>
2019-06-07 03:30:24 +0200

    Temporary change to taskschedule script

--- i3/tasks/task-schedule.sh
+++ i3/tasks/task-schedule.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+source .config/base16-shell/scripts/base16-default-dark.sh
+cd git/taskschedule
+source bin/activate
+python3 __main__.py
+bash
+
 # Run taskwarrior in firejail every second. Taskwarrior has no read-only
 # option, so firejail is used to prevent it from editing and breaking things.
 
-function finish {
-    printf "\e[?25h"
-    bash
-}
-trap finish EXIT
-
-source .config/base16-shell/scripts/base16-default-dark.sh
-printf "\e[?25l"
-while :; do
-    output=$(clear; firejail --overlay-tmpfs task schedule rc.defaultwidth=144 2> /dev/null)
-    echo "$output"
-    sleep 1
-done
+#function finish {
+#    printf "\e[?25h"
+#    bash
+#}
+#trap finish EXIT
+#
+#source .config/base16-shell/scripts/base16-default-dark.sh
+#printf "\e[?25l"
+#while :; do
+#    output=$(clear; firejail --overlay-tmpfs task schedule rc.defaultwidth=144 2> /dev/null)
+#    echo "$output"
+#    sleep 1
+#done
