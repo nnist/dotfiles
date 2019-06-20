aa8fafa921c1d4ad16e51ee00db5b08cd738298f
Nicole Nisters <n.nisters@pm.me>
2019-06-20 13:51:07 +0200

    i3: Rename scrap.md to brain-dump.md

--- i3/tasks/vim-inbox.sh
+++ i3/tasks/vim-inbox.sh
@@ -1,4 +1,4 @@
 source .config/base16-shell/scripts/base16-default-dark.sh
 cd ~/gtd
-vim scrap.md inbox/inbox.md incubate/incubate.md reference/reference.md
+vim brain-dump.md inbox/inbox.md incubate/incubate.md reference/reference.md
 bash
