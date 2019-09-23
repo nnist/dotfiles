b8338869a7ca2253734cd37177cd3c09140791e8
Nicole Nisters <n.nisters@pm.me>
2019-09-23 16:58:24 +0200

    Vim: vimwiki: Use base16 grayscale theme

--- scripts/vimwiki.sh
+++ scripts/vimwiki.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd "$HOME/.vimwiki" || exit
-vim --cmd "silent !source $HOME/.config/base16-shell/scripts/base16-tomorrow.sh" \
+vim --cmd "silent !source $HOME/.config/base16-shell/scripts/base16-grayscale-light.sh" \
     --cmd "set background=light" \
-    -c "execute 'color base16-tomorrow'" \
+    -c "execute 'color base16-grayscale-light'" \
     -c "Goyo" \
     --cmd "let g:gitgutter_enabled=0" \
     index.md
