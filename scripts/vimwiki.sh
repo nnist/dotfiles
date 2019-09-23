d1f902f5fee531b4be0bb6097dc1034abe711b91
Nicole Nisters <n.nisters@pm.me>
2019-09-23 13:52:29 +0200

    Fix vimwiki colorscheme

--- scripts/vimwiki.sh
+++ scripts/vimwiki.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-source "$HOME/.config/base16-shell/scripts/base16-tomorrow.sh"
 cd "$HOME/.vimwiki" || exit
-vim -c Goyo index.md
+vim --cmd "silent !source $HOME/.config/base16-shell/scripts/base16-tomorrow.sh" \
+    --cmd "set background=light" \
+    -c "execute 'color base16-tomorrow'" \
+    -c "Goyo" \
+    --cmd "let g:gitgutter_enabled=0" \
+    index.md
