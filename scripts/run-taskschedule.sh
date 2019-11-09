c0d15a5d9f2d41bf01b5e8e7bb134434e800aaa4
Nicole Nisters <n.nisters@pm.me>
2019-11-09 23:37:32 +0100

    Ignore shellcheck error

--- scripts/run-taskschedule.sh
+++ scripts/run-taskschedule.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC1090
 source "$HOME/.config/base16-shell/scripts/base16-default-dark.sh"
 cd "$HOME/git/taskschedule" || exit
 python3 __main__.py --from today-7days --to today+7days -a
