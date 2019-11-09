b3111b0247a547643a75f39517a530fe44302817
Nicole Nisters <n.nisters@pm.me>
2019-11-10 00:10:56 +0100

    Run taskschedule in firejail

--- scripts/run-taskschedule.sh
+++ scripts/run-taskschedule.sh
@@ -2,4 +2,4 @@
 # shellcheck disable=SC1090
 source "$HOME/.config/base16-shell/scripts/base16-default-dark.sh"
 cd "$HOME/git/taskschedule" || exit
-python3 __main__.py --from today-7days --to today+7days -a
+firejail --net=none --private-dev --shell=none python3 __main__.py --from today-7days --to today+7days -a
