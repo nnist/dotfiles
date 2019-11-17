ae8c963bda12c61831652519cf597df9d2087cca
Nicole Nisters <n.nisters@pm.me>
2019-11-17 22:08:23 +0100

    Allow passing name when creating workspace

--- scripts/create-workspace.sh
+++ scripts/create-workspace.sh
@@ -8,6 +8,12 @@ if [ $# -eq 0 ]; then
 else
     ws_num=$1
     ws_profile=$2
+    ws_name=$3
+fi
+
+# Use the profile name as the workspace name if it is not specified
+if [ -z "$ws_name" ]; then
+    ws_name=$ws_profile
 fi
 
 # Create the workspace and load the profile. If it already exists, switch to it
@@ -16,6 +22,6 @@ if i3-msg -t get_workspaces | grep "$ws_num: $ws_profile"; then
 else
     i3-resurrect restore \
     -d ~/git/dotfiles/i3-resurrect \
-    -w "$ws_num: $ws_profile" \
+    -w "$ws_num · $ws_name" \
     -p "$ws_profile"
 fi
