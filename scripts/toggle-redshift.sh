329d55895f6a3227db67b29b2a1065ac3cd988ea
Nicole Nisters <n.nisters@pm.me>
2020-04-10 04:07:41 +0200

    Always reset redshift gamma even if not running

--- scripts/toggle-redshift.sh
+++ scripts/toggle-redshift.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+redshift -x
 if pgrep -x redshift &> /dev/null; then
-    redshift -x
     killall -q redshift
 else
     redshift -l 52.09:5.11 -b 1.0:1.0 -t 6500:1900 -r -P &> /dev/null 
