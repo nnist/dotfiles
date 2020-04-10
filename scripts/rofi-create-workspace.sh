413fd915c7e1d82f9eb71c425a99e6330822e222
Nicole Nisters <n.nisters@pm.me>
2020-04-10 19:42:04 +0200

    Add history file for workspace name to rofi script

--- scripts/rofi-create-workspace.sh
+++ scripts/rofi-create-workspace.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# Select profle
+HIST_FILE="$HOME/git/dotfiles/rofi/.workspace_name_history"
 PROFILES_DIR="$HOME/git/dotfiles/i3-resurrect/profiles/"
+
+# Create history file if it doesn't exist
+if [ ! -f /tmp/foo.txt ]; then
+    touch "$HIST_FILE"
+fi
+
+# Select profle
 workspaces=$(find "$PROFILES_DIR"*layout.json | sed "s#$PROFILES_DIR##;s#_layout.json##")
 ws_profile=$(echo "$workspaces" | rofi -dmenu -theme base16-default-dark -p "(1/3) Select profile")
 if [ "x$ws_profile" == "x" ]; then
@@ -9,7 +16,7 @@ if [ "x$ws_profile" == "x" ]; then
 fi
 
 # Input workspace name
-ws_name=$(rofi -dmenu -theme base16-default-dark -input /dev/null -p "(2/3) Assign name")
+ws_name=$(rofi -input "$HIST_FILE" -dmenu -theme base16-default-dark -p "(2/3) Assign name")
 if [ "x$ws_name" == "x" ]; then
     exit 0
 fi
@@ -20,7 +27,13 @@ if [ "x$ws_num" == "x" ]; then
     exit 0
 fi
 
+# Load workspace profile
 i3-resurrect restore \
     -d ~/git/dotfiles/i3-resurrect \
     -w "$ws_num · $ws_name" \
     -p "$ws_profile"
+
+# Add workspace name to history
+if ! grep "$ws_name" "$HIST_FILE"; then
+    echo "$ws_name" >>"$HIST_FILE"
+fi
