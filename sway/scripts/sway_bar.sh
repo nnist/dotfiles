a106cb0412330c485875f534799c5e2e46f6eca7
Nicole Nisters <n.nisters@pm.me>
2020-05-03 23:26:31 +0200

    Show date and battery status in swaybar

---
+++ sway/scripts/sway_bar.sh
@@ -0,0 +1,13 @@
+date_formatted=$(date +'%H:%M · %a %-d %b')
+
+battery_charge=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "percentage" | awk '{print $2}')
+battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state" | awk '{print $2}')
+
+if [ $battery_status = "discharging" ];
+then
+    battery_pluggedin='🔋'
+else
+    battery_pluggedin='⚡'
+fi
+
+echo "$battery_pluggedin $battery_charge · $date_formatted "
