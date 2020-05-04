de562956462681c6493c4ac8fa111b04a3d3ad46
Nicole Nisters <n.nisters@pm.me>
2020-05-05 01:56:34 +0200

    Set battery icon

--- sway/scripts/sway_bar.sh
+++ sway/scripts/sway_bar.sh
@@ -3,11 +3,10 @@ date_formatted=$(date +'%H:%M · %a %-d %b')
 battery_charge=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "percentage" | awk '{print $2}')
 battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state" | awk '{print $2}')
 
+battery_icon=''
 if [ $battery_status = "discharging" ];
 then
-    battery_pluggedin='🔋'
-else
-    battery_pluggedin='⚡'
+    battery_icon=''
 fi
 
-echo "$battery_pluggedin $battery_charge · $date_formatted "
+echo "$battery_icon $battery_charge · $date_formatted "
