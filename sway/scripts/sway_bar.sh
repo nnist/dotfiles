e7c87d83674192ed992037c685a1e137ba515847
Nicole Nisters <n.nisters@pm.me>
2020-05-05 21:59:03 +0200

    Show battery icon while charging

--- sway/scripts/sway_bar.sh
+++ sway/scripts/sway_bar.sh
@@ -3,23 +3,26 @@ date_formatted=$(date +'%a %-d %b · %H:%M')
 battery_raw=$(upower --show-info $(upower --enumerate | grep 'BAT'))
 battery_charge=$(echo "$battery_raw" | egrep "percentage" | awk '{print $2}')
 battery_status=$(echo "$battery_raw" | egrep "state" | awk '{print $2}')
-battery_icon=''
+
+if (( "${battery_charge//%}" < 10)); then
+    battery_num=0
+elif (( "${battery_charge//%}" == 100)); then
+    battery_num=10
+else
+    battery_num="${battery_charge:0:1}"
+fi
+battery_icons=(          )
+battery_icon="${battery_icons[battery_num]}"
+battery_charge_icon=""
+
 if [ $battery_status = "discharging" ];
 then
-    if (( "${battery_charge//%}" < 10)); then
-        battery_num=0
-    elif (( "${battery_charge//%}" == 100)); then
-        battery_num=10
-    else
-        battery_num="${battery_charge:0:1}"
-    fi
-    battery_icons=(          )
-    battery_icon="${battery_icons[battery_num]}"
     battery_time="$(echo "$battery_raw" | grep "time\ to\ empty" | awk '{print $4 substr ($5, 0, 1)}')"
 else
+    battery_charge_icon=""
     battery_time="$(echo "$battery_raw" | grep "time\ to\ full" | awk '{print $4 substr ($5, 0, 1)}')"
 fi
-battery_status="$battery_icon $battery_charge ($battery_time)"
+battery_status="$battery_charge_icon$battery_icon $battery_charge ($battery_time)"
 
 vpn_status=''
 if [ -d "/proc/sys/net/ipv4/conf/tun0" ]; then
