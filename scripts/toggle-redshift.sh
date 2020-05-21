4b7aab4113d00ffd147793031597b19bd84a7c00
Nicole Nisters <n.nisters@pm.me>
2020-05-22 01:16:06 +0200

    Run redshift in background

--- scripts/toggle-redshift.sh
+++ scripts/toggle-redshift.sh
@@ -3,5 +3,5 @@ redshift -x
 if pgrep -x redshift &> /dev/null; then
     killall -q redshift
 else
-    redshift -l 52.09:5.11 -b 1.0:1.0 -t 6500:1900 -r -P &> /dev/null 
+    redshift -l 52.09:5.11 -b 1.0:1.0 -t 6500:1900 -r -P &> /dev/null &
 fi
