fa91960721f7c6f3696be2512b19c75e197932a7
Nicole Nisters <n.nisters@pm.me>
2020-05-22 03:06:36 +0200

    Add test script to create workspace

---
+++ sway/scripts/test_create_workspace.sh
@@ -0,0 +1,40 @@
+# TODO Adjust alacritty text size to make more readable on 2x scaling
+# TODO Remove debug titles and marks
+
+ws_num="4"
+ws_name="dev"
+delay=.2
+long_delay=1.0
+
+swaymsg workspace "$ws_num"
+swaymsg rename workspace to "$ws_num · $ws_name"
+
+swaymsg layout splith
+
+firefox --no-remote &> /dev/null &
+sleep $long_delay
+swaymsg border normal
+swaymsg mark num_0
+
+alacritty &> /dev/null &
+sleep $delay
+swaymsg border normal
+swaymsg mark num_1
+
+~/git/dotfiles/scripts/vimwiki-dark &
+sleep $delay
+swaymsg border normal
+swaymsg mark num_2
+
+swaymsg splitv
+
+alacritty &> /dev/null &
+sleep $delay
+swaymsg border normal
+swaymsg mark num_3
+
+alacritty &> /dev/null &
+sleep $delay
+swaymsg border normal
+swaymsg mark num_4
+swaymsg resize set height 30 ppt
