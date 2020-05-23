f56508e2f7596cc4f4a7403d22ed527b016d6ef2
Nicole Nisters <n.nisters@pm.me>
2020-05-23 02:59:43 +0200

    Allow inputting working dir

--- scripts/rofi-create-workspace.sh
+++ scripts/rofi-create-workspace.sh
@@ -10,25 +10,31 @@ fi
 
 # Select profle
 workspaces=$(find "$PROFILES_DIR"*layout.json | sed "s#$PROFILES_DIR##;s#_layout.json##")
-ws_profile=$($LAUNCH_SCRIPT -l | rofi -dmenu -theme base16-default-dark -p "(1/3) Select profile")
+ws_profile=$($LAUNCH_SCRIPT -l | rofi -dmenu -theme base16-default-dark -p "(1/4) Select profile")
 if [ "x$ws_profile" == "x" ]; then
     exit 0
 fi
 
 # Input workspace name
-ws_name=$(rofi -input "$HIST_FILE" -dmenu -theme base16-default-dark -p "(2/3) Assign name")
+ws_name=$(rofi -input "$HIST_FILE" -dmenu -theme base16-default-dark -p "(2/4) Assign name")
 if [ "x$ws_name" == "x" ]; then
     exit 0
 fi
 
 # Input workspace number
-ws_num=$(rofi -dmenu -theme base16-default-dark -input /dev/null -p "(3/3) Assign number")
+ws_num=$(rofi -dmenu -theme base16-default-dark -input /dev/null -p "(3/4) Assign number")
 if [ "x$ws_num" == "x" ]; then
     exit 0
 fi
 
+# Input working directory
+ws_dir=$(readlink -e ~/git/* | rofi -dmenu -theme base16-default-dark -p "(4/4) Set working directory")
+if [ "x$ws_dir" == "x" ]; then
+    exit 0
+fi
+
 # Load workspace profile
-$($LAUNCH_SCRIPT --num $ws_num --name $ws_name)
+$($LAUNCH_SCRIPT --num $ws_num --name $ws_name -d $ws_dir)
 
 # Add workspace name to history
 if ! grep "$ws_name" "$HIST_FILE"; then
