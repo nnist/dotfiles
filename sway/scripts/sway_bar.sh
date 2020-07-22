41955130141c352b5f82d11d7aa8f029a2edf3d2
Nicole Nisters <n.nisters@pm.me>
2020-07-22 15:51:32 +0200

    Fix shellcheck warnings

--- sway/scripts/sway_bar.sh
+++ sway/scripts/sway_bar.sh
@@ -1,8 +1,8 @@
 date_formatted=$(date +'%a %-d %b · %H:%M')
 
-battery_raw=$(upower --show-info $(upower --enumerate | grep 'BAT'))
-battery_charge=$(echo "$battery_raw" | egrep "percentage" | awk '{print $2}')
-battery_status=$(echo "$battery_raw" | egrep "state" | awk '{print $2}')
+battery_raw=$(upower --show-info "$(upower --enumerate | grep 'BAT')")
+battery_charge=$(echo "$battery_raw" | grep -E "percentage" | awk '{print $2}')
+battery_status=$(echo "$battery_raw" | grep -E "state" | awk '{print $2}')
 
 if (("${battery_charge//%/}" < 10)); then
     battery_num=0
@@ -15,7 +15,7 @@ battery_icons=(          )
 battery_icon="${battery_icons[battery_num]}"
 battery_charge_icon=""
 
-if [ $battery_status = "discharging" ]; then
+if [ "$battery_status" = "discharging" ]; then
     battery_time="$(echo "$battery_raw" | grep "time\ to\ empty" | awk '{print $4 substr ($5, 0, 1)}')"
 else
     battery_charge_icon=""
@@ -30,8 +30,8 @@ fi
 
 audio_raw=$(amixer sget Master | grep 'Right:')
 audio_status="婢"
-if [ $(echo $audio_raw | awk -F'[][]' '{ print $4 }') = "on" ]; then
-    audio_volume=$(echo $audio_raw | awk -F'[][]' '{ print $2 }')
+if [ "$(echo "$audio_raw" | awk -F'[][]' '{ print $4 }')" = "on" ]; then
+    audio_volume=$(echo "$audio_raw" | awk -F'[][]' '{ print $2 }')
     audio_status="墳 $audio_volume"
 fi
 
