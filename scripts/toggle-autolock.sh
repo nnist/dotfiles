f5dbcebb39be5fb65e26a869a3ba271b6d0ec145
Nicole Nisters <n.nisters@pm.me>
2020-08-14 00:41:04 +0200

    Rename swayidle toggle script to autolock

--- scripts/toggle-autolock.sh
+++ scripts/toggle-autolock.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if pgrep -x swayidle &>/dev/null; then
     killall -q swayidle
-    notify-send -u normal "Swayidle disabled." -t 1000
+    notify-send -u normal "Autolock disabled." -t 1000
 else
     swayidle -w \
         timeout 300 'swaylock -f -c 181818' \
         timeout 600 'swaymsg "output * dpms off"' \
         resume 'swaymsg "output * dpms on"' \
         before-sleep 'swaylock -f -c 181818' &
-    notify-send -u normal "Swayidle enabled." -t 1000
+    notify-send -u normal "Autolock enabled." -t 1000
 fi
