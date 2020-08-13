2741dec00cda7813bee51b91d2e75fc155ec5a99
Nicole Nisters <n.nisters@pm.me>
2020-08-14 00:22:04 +0200

    Invert image if dark background
    Tesseract seems to have some problems extracting text from dark
    backgrounds. The chosen workaround is to detect whether the background
    is bright or dark, and invert the image if needed.

--- scripts/tesseract-selection.sh
+++ scripts/tesseract-selection.sh
@@ -4,8 +4,18 @@
 
 selection=$(grim -g "$(slurp)" - | base64 -)
 
+background_color_hex=$(echo "$selection" | base64 --decode - | convert -resize 1x1 - txt:- | awk 'FNR==2{print substr($3,2)}')
+background_color_int=$(echo "ibase=16; $background_color_hex" | bc)
+
+if (("$background_color_int" < 8388607)); then
+    invert="-negate -channel RGB"
+else
+    invert=""
+fi
+
 output=$(echo "$selection" |
     base64 --decode - |
+    convert $invert - - |
     convert -colorspace gray -type grayscale -level 60%x85% -sharpen 0x4 - - |
     tesseract - "stdout" 2>/dev/null |
     sed -e 's/\f//')
