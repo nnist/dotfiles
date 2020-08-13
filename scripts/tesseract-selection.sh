a98bf904351131b56d8e16c4800ecd7c2df5c7d3
Nicole Nisters <n.nisters@pm.me>
2020-08-14 00:36:25 +0200

    Add comments for clarity

--- scripts/tesseract-selection.sh
+++ scripts/tesseract-selection.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+# Grab a selection and encode it with base64 to easily pass it on
 selection=$(grim -g "$(slurp)" - | base64 -)
 
+# Get the average background color
 background_color_hex=$(echo "$selection" | base64 --decode - | convert -resize 1x1 - txt:- | awk 'FNR==2{print substr($3,2)}')
+
+# Convert the hex color to int
 background_color_int=$(echo "ibase=16; $background_color_hex" | bc)
 
+# Determine if color is closer to black or closer to white; adjust params accordingly
 if (("$background_color_int" < 8388607)); then
     invert="-negate -channel RGB"
 else
     invert=""
 fi
 
+# Process the selection and pass into tesseract
 output=$(echo "$selection" |
     base64 --decode - |
     convert $invert - - |
     convert -colorspace gray -type grayscale -level 60%x85% -sharpen 0x4 - - |
     tesseract - "stdout" 2>/dev/null |
-    sed -e 's/\f//')
+    sed -e 's/\f//') # Strip form-feed character from output
 
 echo "$output" | wl-copy -n
 
