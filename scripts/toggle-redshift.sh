dd549d5d12236d6453eb925497ef0bb06d5c97d8
Nicole Nisters <n.nisters@pm.me>
2020-08-30 20:35:24 +0200

    Fix redshift toggle script

--- scripts/toggle-redshift.sh
+++ scripts/toggle-redshift.sh
@@ -2,8 +2,8 @@
 if ! [[ $DESKTOP_SESSION == "sway" ]]; then
     redshift -x
 fi
-if pgrep -x redshift &> /dev/null; then
+if pgrep -x redshift &>/dev/null; then
     killall -q redshift
 else
-    redshift -l 52.09:5.11 -b 1.0:1.0 -t 6500:1900 -r -P &> /dev/null &
+    ~/.local/bin/redshift -l 52.09:5.11 -b 1.0:1.0 -t 6500:1900 -r -P &>/dev/null &
 fi
