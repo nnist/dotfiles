26974cd8ef9eb57ac2a8c4bf4cf2cde8f16cba30
Nicole Nisters <n.nisters@pm.me>
2021-10-15 11:24:40 +0200

    Use new anim for lock and suspend

--- scripts/suspend.sh
+++ scripts/suspend.sh
@@ -1,1 +1,1 @@
-bash -c "~/git/sway-utils/sway_utils/effects/overlay_term.sh ~/git/sway-utils/sway_utils/effects/suspend_animation.py && ~/git/sway-utils/sway_utils/effects/crt.py --lock; systemctl suspend"
+bash -c "~/git/sway-utils/sway_utils/effects/overlay_term.sh '~/git/sway-utils/sway_utils/effects/alert.py SUSPEND red' && ~/git/sway-utils/sway_utils/effects/crt.py --lock; systemctl suspend"
